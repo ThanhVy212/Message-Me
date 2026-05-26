9f0c939a5b1e068ddf964bfaee3d72f5f9bab128
Thanh Vỵ <buivy22122004@gmail.com>
2026-05-27 00:33:22 +0700

    chat card, direct message card, group chat card

--- frontend/src/stores/useAuthStore.ts
+++ frontend/src/stores/useAuthStore.ts
@@ -3,6 +3,7 @@ import { toast } from "sonner";
 import { authService } from "@/services/authService";
 import type { AuthState } from "@/types/store";
 import { persist } from "zustand/middleware";
+import { useChatStore } from "./useChatStore";
 
 export const useAuthStore = create<AuthState>()(
   persist(
@@ -18,6 +19,7 @@ export const useAuthStore = create<AuthState>()(
       clearState: () => {
         set({ accessToken: null, user: null, loading: false });
         localStorage.clear();
+        useChatStore.getState().reset();
       },
 
       signUp: async (username, password, email, firstName, lastName) => {
@@ -55,12 +57,14 @@ export const useAuthStore = create<AuthState>()(
           set({ loading: true });
 
           localStorage.clear();
+          useChatStore.getState().reset();
 
           const { accessToken } = await authService.signIn(username, password);
 
           get().setAccessToken(accessToken);
 
           await get().fetchMe();
+          useChatStore.getState().fetchConversations();
 
           toast.success("Chào mừng bạn quay lại");
 
